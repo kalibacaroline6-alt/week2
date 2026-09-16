650d84b324b0f98cbead39f3bfbd216823e4d44f
kalibacaroline6-alt <kalibacaroline6@gmail.com>
2026-09-16 16:20:58 +0200

    Add attributes and methods to BankAccount

--- src/main/java/com/oop/Main.java
+++ src/main/java/com/oop/Main.java
@@ -2,10 +2,19 @@ package com.oop;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         BankAccount KalibasAccount = new BankAccount();
         BankAccount useAccount = new BankAccount();
+
+       KalibasAccount.accountHolder = "Kaliba";
+       KalibasAccount.balance = 100;
+       System.out.println("Before deposit: " + KalibasAccount.balance);
+
+       KalibasAccount.deposit(100);
+
+       System.out.println("After deposit: " + KalibasAccount.balance);
+
+       System.out.println(useAccount.balance);
 
         System.out.println(KalibasAccount == useAccount);
 
